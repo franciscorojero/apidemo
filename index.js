c140a58dc3b4d94cf8b86a56bdcbbb70abdc39e6
Francisco Rojero <frojerod@gmail.com>
2025-11-21 09:18:14 -0600

    Primera versión

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Ruta 1
+app.get('/', (req, res) => {
+  res.send('Bienvenidos al API');
+});
+
+// Ruta 2
+app.get('/mensaje1', (req, res) => {
+  res.send('Aquí va el mensaje 1. ¡Hola desde Express!');
+});
+
+// Ruta 3
+app.get('/mensaje2', (req, res) => {
+  res.send('Mensaje 2: tu API ya funciona 😊');
+});
+
+// Iniciar servidor
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
